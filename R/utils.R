be9db2fa30cb5e9dc9956a008ea6fc5d87a62011
Mariana <mariana.montes@kuleuven.be>
2022-10-13 13:27:20 +0200

    finish off contingency tables slides

---
+++ R/utils.R
@@ -0,0 +1,13 @@
+abcd_o <- function(target, reference,
+                   target_item = "Target item",
+                   others = "Other items") {
+  tibble::tibble(
+    rows = c(target, reference, "Total"),
+    colloc = c("O<sub>11</sub>", "O<sub>12</sub>", "C<sub>1</sub>"),
+    not_colloc = c("O<sub>21</sub>", "O<sub>22</sub>", "C<sub>2</sub>"),
+    total = c("R<sub>1</sub>", "R<sub>2</sub>", "N")
+  ) %>% 
+    kableExtra::kbl(col.names = c(" ", target_item, others, "Total"),
+        escape = FALSE) %>%
+    kableExtra::kable_styling()
+}
