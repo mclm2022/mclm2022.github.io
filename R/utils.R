f03f86eab6d639ce2735a6835f1124c7817297a7
Mariana <mariana.montes@kuleuven.be>
2022-10-19 10:48:21 +0200

    update index and git cheatsheet

--- R/utils.R
+++ R/utils.R
@@ -3,8 +3,8 @@ abcd_o <- function(target, reference,
                    others = "Other items") {
   tibble::tibble(
     rows = c(target, reference, "Total"),
-    colloc = c("O<sub>11</sub>", "O<sub>12</sub>", "C<sub>1</sub>"),
-    not_colloc = c("O<sub>21</sub>", "O<sub>22</sub>", "C<sub>2</sub>"),
+    colloc = c("O<sub>11</sub>", "O<sub>21</sub>", "C<sub>1</sub>"),
+    not_colloc = c("O<sub>12</sub>", "O<sub>22</sub>", "C<sub>2</sub>"),
     total = c("R<sub>1</sub>", "R<sub>2</sub>", "N")
   ) %>% 
     kableExtra::kbl(col.names = c(" ", target_item, others, "Total"),
