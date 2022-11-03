66d3c93e2200b97a70edd855b24e7eab7d86c91e
Mariana <mariana.montes@kuleuven.be>
2022-11-03 13:33:35 +0100

    add logistic regression slides and code

--- resources/logistic-regression.R
+++ resources/logistic-regression.R
@@ -3,6 +3,7 @@
 # Libraries ####################################################################
 library(tidyverse)
 library(kableExtra)
+library(ggeffects)
 options(digits = 3) # print up to 3 digits
 
 # Custom functions #############################################################
@@ -22,18 +23,24 @@ kb_style <- function(df) {
 # Logistic regression ##########################################################
 ## Set up data =================================================================
 set.seed(2022)
-gonna <- tibble(gt = rep("gonna", 50), comp_length = rnorm(50, 6, 0.8))
-going_to <- tibble(gt = rep("going_to", 50), comp_length = rnorm(50, 3, 1.2))
+gonna <- tibble(variant = rep("gonna", 50), comp_length = rnorm(50, 6, 0.8),
+                register = sample(c("formal", "informal"), 50, replace = TRUE, prob = c(0.1, 0.9)))
+going_to <- tibble(variant = rep("going_to", 50), comp_length = rnorm(50, 3, 1.2),
+                   register = sample(c("formal", "informal"), 50, replace = TRUE, prob = c(0.7, 0.3)))
 gt <- bind_rows(gonna, going_to) %>%
   mutate(
     comp_length = if_else(comp_length > 1, comp_length, 1),
-    gt = fct_relevel(gt, "gonna"), gt_num = as.numeric(gt)-1
+    variant = fct_relevel(variant, "gonna"),
+    variant_num = as.numeric(variant)-1,
+    register = fct_relevel(register, "informal"),
+    source = factor(sample(letters, n(), replace = TRUE))
   )
-m3 <- glm(gt ~ comp_length, data = gt, family = binomial(logit))
-gt$fit <- m3$fitted.values
-
+m1 <- glm(variant ~ comp_length, data = gt, family = binomial(logit))
+gt$fit1 <- m1$fitted.values
+m2 <- glm(variant ~ comp_length + register, data = gt, family = binomial(logit))
+gt$fit2 <- m2$fitted.values
 ## Plot ========================================================================
-gonna_plot <- ggplot(gt, aes(x = comp_length, y = gt_num)) +
+gonna_plot <- ggplot(gt, aes(x = comp_length, y = variant_num)) +
   geom_point(size = 3) + ylim(0, 1) +
   labs(x = "Length of complement", y = "Choice of construction") +
   scale_y_continuous(breaks = c(0, 1), labels = c("gonna", "going to")) +
@@ -46,10 +53,24 @@ gonna_plot +
 
 ### With logistic fitting ----
 gonna_plot +
-  geom_line(aes(y = fit), color = "goldenrod", size = 2)
+  geom_line(aes(y = fit1), color = "goldenrod", size = 2)
 
 ## Model =======================================================================
-summary(m3)
+summary(m2)
+
+### extract intercept ----
+intercept <- m2$coefficients[[1]] # log odds
+intercept
+odds <- exp(intercept)
+odds # odds
+odds/(odds + 1) # probabilities
+
+### extract a predictor coefficient
+cl_lor <- m2$coefficients[["comp_length"]] # log odds ratio
+exp(cl_lor) # odds ratio
+
+cl_reg <- m2$coefficients[["registerformal"]]
+exp(cl_reg)
 
 # Probabilities, odds and logits ###############################################
 ## Set up simulated data =======================================================
@@ -70,6 +91,26 @@ with_x <- tibble(
   odds = exp(logit),
   P = odds/(1+odds))
 
-## Plot =======================================================================
+## Plot ========================================================================
 lnplot(with_x, x, logit, "logit ~ X")
 lnplot(with_x, x, P, "P ~ X")
+
+# Evaluation ###################################################################
+set.seed(0)
+pred_power <- tibble(
+  gt0 = sample(filter(gt, variant == "gonna")$fit2, 15, replace = FALSE),
+  gt1 = sample(filter(gt, variant == "going_to")$fit2, 15, replace = FALSE)) %>% 
+  mutate(C = gt1 > gt0, D = gt0 > gt1, T = gt1 == gt0)
+pred_power
+# Interactions #################################################################
+m3 <- glm(variant ~ comp_length*register, data = gt, family = binomial(logit))
+summary(m3)
+ggpredict(m3, c("comp_length", "register"))
+
+# Random effects ###############################################################
+m4 <- lme4::glmer(variant ~ comp_length*register + (1 | source), data = gt,
+                  family = binomial)
+summary(m4)
+
+## Comparison ==================================================================
+performance::compare_performance(m1, m2, m3, m4, metrics = "AIC")
